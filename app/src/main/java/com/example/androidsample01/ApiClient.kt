b60a111ce4066ec53b8a94fec61a50349c0e1405
GreifGele <greifen.gelegenheit@gmail.com>
2023-06-18 11:11:03 +0900

    SpringSample01 と、ログイン → Cookie認証しつつ posttest リクエスト → 200 ok まで確認

--- app/src/main/java/com/example/androidsample01/ApiClient.kt
+++ app/src/main/java/com/example/androidsample01/ApiClient.kt
@@ -1,34 +1,45 @@
 package com.example.androidsample01
 
-import android.util.Log
-import okhttp3.Call
+import android.webkit.CookieManager
 import okhttp3.Callback
+import okhttp3.Cookie
+import okhttp3.CookieJar
+import okhttp3.HttpUrl
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
-import okhttp3.Response
-import okio.IOException
+
 
 class ApiClient {
-    public fun Call(service: String, body: RequestBody) {
+    public fun call(service: String, body: RequestBody, cb : Callback) {
         //var url = R.string.srv_base_url.toString() + service
         var url = "http://10.0.2.2:8080/$service"
         var req = Request.Builder()
             .url(url)
+            .addHeader("User-Agent", "Mobile") // for simulator
             .post(body).build()
-        var client = OkHttpClient()
-        var call = client.newCall(req)
-        return call.enqueue(object : Callback {
-            override fun onResponse(call: Call, response: Response) {
-                // Responseの読み出し
-                val responseBody = response.body?.string().orEmpty()
-                // 必要に応じてCallback
+        var client = OkHttpClient().newBuilder().cookieJar(getCookieJar()).build()
+        return client.newCall(req).enqueue(cb)
+    }
+
+    private fun getCookieJar() : CookieJar {
+        return object : CookieJar {
+            override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
+                val cookieManager = CookieManager.getInstance()
+                for (cookie in cookies) {
+                    cookieManager.setCookie(url.toString(), cookie.toString())
+                }
             }
 
-            override fun onFailure(call: Call, e: IOException) {
-                Log.e("Error", e.toString())
-                // 必要に応じてCallback
+            override fun loadForRequest(url: HttpUrl): List<Cookie> {
+                val cookieManager = CookieManager.getInstance()
+                val cookies: MutableList<Cookie> = ArrayList()
+                val cookieStr = cookieManager.getCookie(url.toString())
+                if (cookieStr != null) {
+                    cookies.add(Cookie.parse(url, cookieStr)!!)
+                }
+                return cookies
             }
-        })
+        }
     }
 }
--- app/src/main/java/com/example/androidsample01/MainActivity.kt
+++ app/src/main/java/com/example/androidsample01/MainActivity.kt
@@ -5,7 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
+import okhttp3.Call
+import okhttp3.Callback
 import okhttp3.FormBody
+import okhttp3.Response
+import okio.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,11 +20,33 @@ class MainActivity : AppCompatActivity() {
     fun onClickLogin(view: View) {
         Toast.makeText(this.applicationContext,"button clicked", Toast.LENGTH_SHORT).show();
         Log.i("login", "do");
+
         var body = FormBody.Builder()
             .add("username", "user1")
             .add("password", "user1Pass").build()
-        //var res = ApiClient().Call(R.string.srv_login.toString(), body)
-        var res = ApiClient().Call("login_proc", body)
-        Log.i("login", res.toString())
+
+        var cbLogin = object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                val responseBody = response.body?.string().orEmpty()
+
+                var cbTest = object : Callback {
+                    override fun onResponse(call: Call, response: Response) {
+                        val responseBody = response.body?.string().orEmpty()
+                    }
+
+                    override fun onFailure(call: Call, e: IOException) {
+                        Log.e("Error", e.toString())
+                    }
+                }
+
+                ApiClient().call("api/posttest", FormBody.Builder().build(), cbTest)
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Error", e.toString())
+            }
+        }
+
+        ApiClient().call("login_proc", body, cbLogin)
     }
 }
